19ad957553f7679a97f1b5707b5788870bcc5c93
Irtaza khalid <irtazakhalidll@gmail.com>
2024-01-28 17:34:03 +0500

    this is a calculator

---
+++ calculator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Calculator {
+public:
+    // Constructor
+    Calculator() ;
+
+    // Function to add two numbers
+    double add() ;
+
+    // Function to subtract two numbers
+    double subtract() ;
+
+    // Function to multiply two numbers
+    double multiply() ;
+
+    // Function to divide two numbers
+    double divide() ;
+};
+
+int main() {
+   
+
+    return 0;
+}
